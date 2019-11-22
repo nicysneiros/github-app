b78b9b84caa61edd812e490b63f31f756721f74e
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-22 19:50:15 -0300

    Creates UserSearchResult component

---
+++ src/components/UserSearchResult/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from "react-bootstrap/Container";
+
+
+export default class UserSearchResult extends React.Component {
+  render() {
+    return(
+      <Container>
+
+      </Container>
+    );
+  }
+}
