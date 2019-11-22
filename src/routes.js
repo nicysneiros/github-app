8fdb75398fd54845ac5866ffd5decf8aac95d4a4
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-22 19:48:55 -0300

    Implements app routes

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import UserSearch from './components/UserSearch';
+import UserDetail from './components/UserDetail';
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/users/:userId">
+            <UserDetail />
+          </Route>
+          <Route path="/">
+            <UserSearch />``
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
