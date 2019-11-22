254a9c9debf3153a87060ede0c52f48d05c17897
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-22 19:49:23 -0300

    Install react bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import './index.css';
 import App from './App';
 import rootReducer from './store/reducers';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 
 const store = createStore(rootReducer);
