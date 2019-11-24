23ed4338a60248f297916630a9295f9784ea165d
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-24 16:41:02 -0300

    Implements user search result component

---
+++ src/components/UserSearch/searchResult.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import ListGroup from 'react-bootstrap/ListGroup';
+import Alert from 'react-bootstrap/Alert';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+class SearchResult extends React.Component {
+
+  renderUsers() {
+    if(this.props.users) {
+      return this.props.users.map(user => {
+        return (
+          <ListGroup.Item action key={user.id} href={`/users/${user.id}`}>
+            <Row>
+              <Col xs="auto"><img className="user-avatar" src={user.avatar_url} height="48"/></Col>
+              <Col className="user-login">{user.login}</Col>
+            </Row>
+          </ListGroup.Item>
+        );
+      });
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Alert variante='danger'>{this.props.error}</Alert>
+        <ListGroup>{this.renderUsers()}</ListGroup>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = store => ({
+  users: store.usersState.users,
+  error: store.usersState.error
+});
+
+export default connect(mapStateToProps)(SearchResult);
