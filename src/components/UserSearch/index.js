541c2fc90e83be32a7ca78be231e003129ad9d9d
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-24 16:41:58 -0300

    Adds user search result component to user search component

--- src/components/UserSearch/index.js
+++ src/components/UserSearch/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Container from "react-bootstrap/Container";
 
 import SearchForm from './searchForm';
+import SearchResult from './searchResult';
 
 
 export default class UserSearch extends React.Component {
   render() {
     return (
       <Container>
-        <h1>User Search</h1>
+        <h1>Github User Search</h1>
         <SearchForm />
+        <SearchResult />
       </Container>
     );
   }
