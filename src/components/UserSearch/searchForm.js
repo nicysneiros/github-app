08bc8370976e591d12eaf18d8eaa63d64b8d75ff
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-24 15:29:27 -0300

    Implements handler for user search click

--- src/components/UserSearch/searchForm.js
+++ src/components/UserSearch/searchForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { searchUsers, searchUsersSuccess, searchUsersError } from '../../store/actions';
 
 
-export default class SearchForm extends React.Component {
+const GITHUB_API_TOKEN = `${process.env.REACT_APP_GITHUB_API_TOKEN}`;
+const GITHUB_API_USER = `${process.env.REACT_APP_GITHUB_API_USER}`;
+
+
+class SearchForm extends React.Component {
   state = {
     searchTerm: ''
   }
@@ -16,8 +24,22 @@ export default class SearchForm extends React.Component {
   }
 
   handleSearchClick = () => {
-    // This function should dispatch search user action
-    console.log("Search for: ", this.state.searchTerm);
+    this.props.searchUsers();
+
+    const headers = new Headers({
+      'Authorization': 'Basic ' + btoa(`${GITHUB_API_USER}:${GITHUB_API_TOKEN}`)
+    });
+
+    const url = 'https://api.github.com/search/users?q=' + this.state.searchTerm;
+
+    fetch(url, {method: 'GET', headers: headers})
+      .then(response => response.json())
+      .then(response => {
+        this.props.searchUsersSuccess(response);
+      })
+      .catch(error => {
+        this.props.searchUsersError(error);
+      });
   }
 
   render() {
@@ -44,3 +66,13 @@ export default class SearchForm extends React.Component {
     )
   }
 }
+
+const mapStateToProps = store => ({
+  isLoading: store.usersState.isLoading
+});
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ searchUsers, searchUsersSuccess, searchUsersError }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
