3f5ac88c09e443da4d14925836d8f186439289e1
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-11-24 15:25:01 -0300

    Creates user search actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,30 @@
-import { CLICK_UPDATE_VALUE } from './actionTypes';
+import {
+    SEARCH_USERS,
+    SEARCH_USERS_SUCCESS,
+    SEARCH_USERS_ERROR
+} from '../actionTypes';
 
-export const clickButton = value => ({
-    type: CLICK_UPDATE_VALUE,
-    newValue: value
-});
+export const searchUsers = value => {
+    return {
+        type: SEARCH_USERS,
+        isLoading: true
+    };
+}
+
+export const searchUsersSuccess = value => {
+    const users = value.items;
+    return {
+        type: SEARCH_USERS_SUCCESS,
+        isLoading: false,
+        users: users
+    };
+}
+
+export const searchUsersError = value => {
+    const error = value.message;
+    return {
+        type: SEARCH_USERS_ERROR,
+        isLoading: false,
+        error: error
+    }
+}
