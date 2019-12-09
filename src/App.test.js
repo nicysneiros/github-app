41c27e69181a6155c660f5c6819fd44dc7b49db7
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2019-12-09 10:32:33 -0300

    Fixes test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from "react-dom";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import configureStore from 'redux-mock-store';
 
 import App from './App';
-import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer)
+
+const mockStore = configureStore([]);
+const store =  mockStore({
+  usersState: {
+    isLoading: false,
+    users: []
+  }
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Provider store={store}><App /></Provider>, div);
+  unmountComponentAtNode(div);
 });
